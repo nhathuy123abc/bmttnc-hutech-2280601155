78e76492ea4c320b30817022b0a267b0c1e76860
nhathuy123abc <nhathuy2004dk@gmail.com>
2025-03-25 11:06:05 +0700

    Add files via upload

--- bmttnc-hutech-2280601155/bmttnc-hutech-2280601155/lab-03/caesar_cipher.py
+++ bmttnc-hutech-2280601155/bmttnc-hutech-2280601155/lab-03/caesar_cipher.py
@@ -15,15 +15,14 @@ class MyApp(QMainWindow):
         url = "http://127.0.0.1:5000/api/caesar/encrypt"
         payload = {
             "plain_text": self.ui.txt_plain_text.toPlainText(),
-            "key": self.ui.txt_key.toPlainText()
+            "key": self.ui.txt_key.text()
         }
         try:
             response = requests.post(url, json=payload)
             if response.status_code == 200:
                 data = response.json()
-                # self.ui.txt_cipher_text.setText(data["encrypted_message"])
-                self.ui.txt_cipher_text.setPlainText(data["encrypted_message"])
-                
+                self.ui.txt_cipher_text.setText(data["encrypted_message"])
+
                 msg = QMessageBox()
                 msg.setIcon(QMessageBox.Information)
                 msg.setText("Encrypted Successfully")
@@ -37,14 +36,14 @@ class MyApp(QMainWindow):
         url = "http://127.0.0.1:5000/api/caesar/decrypt"
         payload = {
             "cipher_text": self.ui.txt_cipher_text.toPlainText(),
-            "key": self.ui.txt_key.toPlainText()
+            "key": self.ui.txt_key.text()
         }
         try:
             response = requests.post(url, json=payload)
             if response.status_code == 200:
                 data = response.json()
-                self.ui.txt_plain_text.setPlainText(data["decrypted_message"])
-                
+                self.ui.txt_plain_text.setText(data["decrypted_message"])
+
                 msg = QMessageBox()
                 msg.setIcon(QMessageBox.Information)
                 msg.setText("Decrypted Successfully")
--- bmttnc-hutech-2280601155/bmttnc-hutech-2280601155/lab-03/cipher/rsa/rsa_cipher.py
+++ bmttnc-hutech-2280601155/bmttnc-hutech-2280601155/lab-03/cipher/rsa/rsa_cipher.py
@@ -33,6 +33,7 @@ class RSACipher:
     def sign(self, message, key):
         return rsa.sign(message.encode('ascii'), key, 'SHA-1')
 
+# SHA-256, SHA-512
     def verify(self, message, signature, key):
         try:
             return rsa.verify(message.encode('ascii'), signature, key,) == 'SHA-1'
